a4b12633acea5f54c3817e79b10aafdbf5ce3cda
alexandru1985 <alexandru_panzaru@yahoo.com>
2015-04-01 17:25:04 +0300

    proiect

--- siruri/interface1.php
+++ siruri/interface1.php
@@ -1,11 +1,11 @@
 <?php
 
-class MyThings implements Countable
+class Numbers implements Countable
 {
 
     public function count() {
         return '1,2,3';
     }
 }
-$object= new MyThings();
+$object= new Numbers();
 echo $object->count();
