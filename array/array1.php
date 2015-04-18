115d065d4591da605af1eebe5c77d703c445249f
alexandru1985 <alexandru_panzaru@yahoo.com>
2015-04-18 11:33:15 +0300

    proiect

--- array/array1.php
+++ array/array1.php
@@ -1,11 +1,21 @@
 <?php
 
+
 $names = array ('Doe', 50);
-
 print_r($names);
-
 // output Array ( [0] => Doe [1] => 50 )
-
 echo $names[0];
-
 // output Doe. [0] reprezinta indexul acestui array numeric.
+
+
+
+
+
+
+
+
+$var= 10 +"string";
+
+echo $var;
+
+// output 10
