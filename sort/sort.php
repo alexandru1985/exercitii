9f5d396021d8a77d90922da31b0e5a3c634fac70
alexandru1985 <alexandru_panzaru@yahoo.com>
2015-06-03 22:42:16 +0300

    proiect

--- sort/sort.php
+++ sort/sort.php
@@ -1,16 +1,14 @@
 <?php
 
-$numbers = array(1,3,5,7,9,11,13,15);
+$numbers = array(1, 3, 5, 7, 9, 11, 13, 15);
 rsort($numbers);
 $arraylenght = count($numbers);
-for($x=0;$x<$arraylenght;$x++){
-    
+for ($x = 0; $x < $arraylenght; $x++) {
+
     echo $numbers[$x];
     echo "<br>";
-    
-    
 }
- $a="carte";
- $b="arte";
- 
- echo strpos($a,$b);
+$a = "carte";
+$b = "arte";
+
+echo strpos($a, $b);
