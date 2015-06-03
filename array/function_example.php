8cdc6feb017c98b013024244055c561cc3d7bc08
alexandru1985 <alexandru_panzaru@yahoo.com>
2015-06-03 22:38:52 +0300

    proiect

---
+++ array/function_example.php
@@ -0,0 +1,8 @@
+<?php
+function adunare($a,$b) {
+$suma=$a+$b;
+return $suma;
+}
+$rezultat=adunare(5,16);
+echo $rezultat;
+?>
---
+++ array/test.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function myTest() {
+     static $x = 0;
+     echo $x;
+     $x++;
+}
+
+myTest();
+echo "<br>";
+myTest();
+echo "<br>";
+myTest();
+?>  
+
+</body>
+</html>
+
+<?php 
+$names = array ('Doe', 50);
+file_put_contents('example.txt',$names);
