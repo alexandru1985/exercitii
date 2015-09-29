04a7d882e75df47d0a58e4a7446f1c17625a8890
alexandru1985 <alexandru_panzaru@yahoo.com>
2015-09-29 21:36:51 +0300

    proiect

--- tutoriale/function.php
+++ tutoriale/function.php
@@ -7,3 +7,28 @@ function text()
 }
 
 echo text();
+
+
+$y = 3;
+
+function modifica($y){
+
+   echo $y++;
+    
+}
+
+echo modifica($y);
+
+// Output 3
+
+$y = 3;
+
+function modify($y){
+
+   echo $y++;
+    
+}
+
+echo modify(5);
+
+// Output 5
