83c3708254202ef6d84eacfd8405f7d9690a0861
alexandru1985 <alexandru_panzaru@yahoo.com>
2015-10-25 15:13:46 +0200

    proiect

---
+++ front_end/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Data</title>
+        <link rel="stylesheet" href="css/style.css" type="text/css"/>
+        <link rel="stylesheet" href="css/reset.css" type="text/css"/>
+        <link rel="stylesheet" href="css/960_24_col.css" type="text/css"/>
+        <link rel="stylesheet" href="css/text.css" type="text/css"/>
+    </head>
+    <body>
+        <header>
+            <h1>Site name</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">HOME</a>
+                    </li>
+                    <li>
+                        <a href="#">ABOUT</a>
+                    </li>
+                    <li>
+                        <a href="#">PORTFOLIO</a>
+                    </li>
+                    <li>
+                        <a href="#">BLOG</a>
+                    </li>
+                    <li>
+                        <a href="#">CONTACT</a>
+                    </li>
+                </ul>
+            </nav>
+            <menu>
+                <ul id="headSocial">
+                    <li>
+                        <a href="#">
+                            <img src ="img/icons/facebook.png" alt="Facebook" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src ="img/icons/dribble.png" alt="Dribble" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src ="img/icons/twitter.png" alt="Twitter" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src ="img/icons/flickr.png" alt="Flickr" />
+                        </a>
+                    </li>
+                </ul>
+            </menu>
+            <form action="" method="get">
+                <p>
+                    Search: <input type ="text" name="search" value="" id="search" />
+                </p>
+            </form>
+        </header>
+        <div id="banner-wrap">
+            <div id="banner">
+                <h1>WELCOME TO OUR SITE</h1>
+                <div id="featured">
+                    <img src ="img/featuredImg1.jpg" alt="Featured" />
+                    <img src ="img/featuredImg2.jpg" alt="Featured" />
+                </div>
+                <p>Lorem ipsum dolor sit amet, postea rationibus eos ex, vel ea facer mediocrem, pri cu stet disputando.
+                </p>
+                <p>Lorem ipsum dolor sit amet, postea rationibus eos ex, vel ea facer mediocrem, pri cu stet disputando.
+                </p>
+                <button>LEARN MORE</button>
+                <button>OUR WORK</button>
+            </div>
+            <p id="breadcrumbs"> You're here: <a href="#">Home</a> > <a href="#">Welcome</a></p>     
+       </div> <!--  end banner-warp-->
+        <div id="main">
+            <div id="primary">
+                <h3>From the blog</h3>
+                    <article>
+                        <h2>
+                             <a href="#">Blog Post Name Here</a>
+                             <h6>December 8th, 2010 | <a href="#">10 Comments</a></h6>
+                        </h2>
+                    </article>
+                <aside></aside>
+            </div>
+        </div>
+
+        <script> src = "https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"
+            ></script>
+    </body>
+</html>
